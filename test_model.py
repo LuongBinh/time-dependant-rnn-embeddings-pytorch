b0969dbbf3ca20ffac8f8476708a81415bdd48ed
LuongBinh <luongbinh067@gmail.com>
2023-12-31 11:40:59 +0700

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,17 +1,34 @@
-import torch
-import torch.nn as nn
-from time_embedding import TimeEmbedding
+# create ModelTest
 class ModelTest(nn.Module):
     def __init__(self):
         super(ModelTest, self).__init__()
-        self.time_emb = TimeEmbedding(25, 40)
-
+        self.time_emb = TimeEmbedding(20, 64)
     def forward(self, x):
         emb = self.time_emb(x)
         return emb
-model = ModelTest()
-# Testing the model
-input_tensor = torch.rand(7, 25)
-results = model(input_tensor)
-results
-assert results.shape == torch.Size([7, 25, 40])
+model_pt = ModelTest()
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model_pt.parameters(), lr=1e-4)
+
+# generated sample dataset and using MSE and Adam optimizer
+inputs = torch.rand(100, 20)
+targets = torch.rand(100, 20, 64)
+dataset = TensorDataset(inputs, targets)
+data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+num_epochs = 15  #15 loops for example
+for epoch in range(num_epochs):
+    total_loss = 0.0
+    num_batches = 0
+
+    for inputs, targets in data_loader:
+        outputs = model_pt(inputs)
+        loss = criterion(outputs, targets)
+        total_loss += loss.item()
+        num_batches += 1
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    avg_loss = total_loss / num_batches
+    print(f'Epoch [{epoch+1}/{num_epochs}], Average MSE: {avg_loss}')
